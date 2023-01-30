6907c0ec3b2db3792c5d626b672833cf7e6063d0
tunayzc <120184420+tunayzc@users.noreply.github.com>
2023-01-30 14:27:09 +0300

    Add project files.

---
+++ Checkers1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Checkers1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Button button = new Button();
+            //button.Width = 50;
+            //button.Height = 50;
+            //button.Text = "My button";
+            //this.Controls.Add(button);
+                            //  BU ŞEKİLDE TEK TEK EKLEMEK YERİNE ARRAY KULLANARAK YAPABİLİRİZ
+            Button[,] buttons = new Button[8,8];
+            int top=0;
+            int left=0;
+            for (int i = 0; i <= buttons.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= buttons.GetUpperBound(1); j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Left= left;
+                    left += 50;
+                    buttons[i, j].Top= top;
+                    if ((i+j)%2==0)
+                    {
+                        buttons[i, j].BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        buttons[i, j].BackColor = Color.White;
+                    }
+                    
+                    this.Controls.Add(buttons[i,j]);
+                }
+                top += 50;
+                left= 0;
+            }
+
+            
+        }
+    }
+}
